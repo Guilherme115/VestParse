2ac577db2bd7136eebe78a821aa2812ca9731197
Guilherme <Guilhermesantos.2008@outlook.com.br>
2025-07-12 21:28:06 -0300

    Criada : ENUM para facilitar a classifação no BANCO DE DADOS.

---
+++ src/main/java/com/example/vestparse/model/Category.java
@@ -0,0 +1,12 @@
+package com.example.vestparse.model;
+
+public enum Category {
+        PORTUGUES,
+        MATEMATICA,
+        CIENCIAS,
+        GEOGRAFIA,
+        HISTORIA,
+        INGLES,
+        REDACAO
+    }
+
