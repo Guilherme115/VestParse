e28265ee13bf8032dcc9c3fb470cd55f578c2c4f
Guilherme <Guilhermesantos.2008@outlook.com.br>
2025-07-18 20:36:50 -0300

    Repository cleaned.

--- src/main/java/com/example/vestparse/model/QuestionsRepository.java
+++ src/main/java/com/example/vestparse/model/QuestionsRepository.java
@@ -3,6 +3,12 @@ package com.example.vestparse.model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface QuestionsRepository extends JpaRepository<QuestionRelation,Long> {
+
+
+
 }
